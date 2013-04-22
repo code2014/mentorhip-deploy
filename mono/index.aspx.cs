5c41a1e94cbb5db6bde83d433ad77965358cdc7a
suhanma <suhanma@gmail.com>
2013-04-22 08:07:07 -0500

    4

--- mono/index.aspx.cs
+++ mono/index.aspx.cs
@@ -1,8 +1,8 @@
 using System;
-
+using System.Web.UI;
 namespace mono
 {
-	public partial class index : System.Web.UI.Page
+	public partial class index : Page
 	{
 
         protected void Page_Load(object sender, EventArgs e)
